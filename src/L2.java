41f3bce1aa844014c8ef604367107661d1f133b1
aceveric000 <Aceveric000@warren.k12.in.us>
2021-08-04 11:51:24 -0400

    Completed L1 and L2

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,15 @@
 public class L2 {
 
     //add the main method header
+
+    public static void main(String[] args){
+        String Cardholder="Richard";
+        double balance=30000;
+        int Cardnumber = 1234567890;
+        System.out.println("Richard");
+        System.out.println(Cardnumber);
+        System.out.println(balance);
+    }
+
+
 }
