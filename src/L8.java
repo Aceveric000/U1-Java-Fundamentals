af2f32ac60b047a44b2dd5017103b00e8c1a7058
aceveric000 <Aceveric000@warren.k12.in.us>
2021-08-30 11:54:59 -0400

    l8 l9

--- src/L8.java
+++ src/L8.java
@@ -1,32 +1,71 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class L8 {
 
     public static void main(String[] args) {
-        String name;
+
+        Scanner read = new Scanner(System.in);
+
+        /*String name;
         int hours;
         double payRate;
         double grossPay;
 
         //Create a Scanner object to read input
-        Scanner read = new Scanner(System.in);
+       // Scanner read = new Scanner(System.in);
 
         //Get the user's name
-        System.out.println("What is your name?");
-        name = read.nextLine();
+       // System.out.println("What is your name?");
+       // name = read.nextLine();
 
         //Get the number of hours worked
-        System.out.println("How many hours did you work?");
-        hours = read.nextInt();
+        //System.out.println("How many hours did you work?");
+       // hours = read.nextInt();
 
         //Get the user's hourly rate
-        System.out.println("What is your hourly pay rate?");
-        payRate = read.nextDouble();
+       // System.out.println("What is your hourly pay rate?");
+        //payRate = read.nextDouble();
 
         //Calculate the gross pay
-        grossPay = hours * payRate;
+       // grossPay = hours * payRate;
 
-        System.out.println("Hello " + name);
-        System.out.println("Your gross pay is $" + grossPay);
+       // System.out.println("Hello " + name);
+       // System.out.println("Your gross pay is $" + grossPay);
+
+
+    // BugaDoo #1 - Hanging carriage
+
+
+         */
+    /*int age;
+    String name;
+
+    System.out.println("what is your age");
+    age = read.nextInt();
+
+    System.out.println("what is your name");
+    name = read.next();
+
+        System.out.println("when you graduate bro");
+        int year = read.nextInt();
+
+        read.nextLine(); // Eats up the hanging
+
+        System.out.println("hello " + name + "you are" + age + "years old!" );
+
+    // bugaboo #2 - tokeN
+
+     */
+        String firstName;
+        String lastName;
+
+        System.out.println( " What is your full name with age? ");
+        firstName = read.nextLine();
+        lastName = read.next();
+        String age = read.nextLine();
+
+        System.out.println("hello, " + firstName + " " + lastName + "you are " + age + " years old");
+
     }
 }
